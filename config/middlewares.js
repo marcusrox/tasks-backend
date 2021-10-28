d785854842aa0908e558f36ee58188c388ec04bf
Marcus Moreira de Souza <marcus.moreira@gmail.com>
2021-10-28 15:46:08 -0300

    fim!

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,4 +6,5 @@ module.exports = app => {
     app.use(cors({
         origin: '*'
     }))
+
 }
--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express()
 const db = require('./config/db')
 const consign = require('consign');
 
-consign().then('./config/middlewares.js').into(app)
+consign()
+    .then('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
 app.db = db
 
