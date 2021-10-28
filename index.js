2ff78426093edc459fc278045dbabeb6b4b19d4b
Marcus Moreira <marcus.moreira@gmail.com>
2021-10-28 03:21:04 -0300

    esposa encerrou expediente

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const db = require('./config/db')
+const consign = require('consign');
+
+consign().then('./config/middlewares.js').into(app)
+
+app.db = db
 
 app.get('/', (request, response) => {
-    response.status(200).send('Meu backend')
+    response.status(200).send('Meu backend ' + new Date())
 })
 
 app.listen(3001, () => {
--- migrations/20211028053705_create_table_tasks.js
+++ migrations/20211028053705_create_table_tasks.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('tasks', table => {
+        table.increments('id').primary()
+        table.string('desc').notNull()
+        table.datetime('estimateAt').notNull()
+        table.datetime('doneAt')
+        //table.integer('userId').notNull()
+        //table.foreign('userId').references('id').inTable('users')
+        table.integer("userId", 11).unsigned().references("id").inTable("users")
+    })  
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('tasks')
 };
